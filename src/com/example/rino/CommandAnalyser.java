9844a8c6be7a262722a91df4e8258b2f6c8149a7
Nicolas <nickolas.ivanov@gmail.com>
2013-03-05 14:48:13 +0400

    outdated blocks are removed

--- src/com/example/rino/CommandAnalyser.java
+++ src/com/example/rino/CommandAnalyser.java
@@ -29,40 +29,42 @@ public class CommandAnalyser extends Activity {
 		Log.d(MainActivity.TAG, this.getLocalClassName() + ": command = '" + command + "'");
 		
 		
-		// Applications' launcher should be here 
-		// Classes 'pattern' and 'matcher' should be used for command analyzing 
-		
-		InputStream patternsStream = this.getApplicationContext().getResources().openRawResource(R.raw.patterns);
+		InputStream patternsStream = this.getApplicationContext().getResources().openRawResource(R.raw.patterns);  // Why not to buffer input?
 		BufferedReader patternsReader = new BufferedReader(new InputStreamReader(patternsStream));
 		
 		try {
 			String line;
 			while ((line = patternsReader.readLine()) != null) {
 				String rawPattern = line;
-				Matcher commonMatcher = commonPattern.matcher(rawPattern);
-				
+				Matcher commonMatcher = commonPattern.matcher(rawPattern);			
 				Log.d(MainActivity.TAG, this.getLocalClassName() + ": commonMatcher = '" + commonMatcher.matches() + "'" + rawPattern);
-				if (commonMatcher.matches()){
+				
+				if (commonMatcher.matches()) {
 					Log.d(MainActivity.TAG, this.getLocalClassName() + ": group(1) = '" + commonMatcher.group(1) + 
 							"' group(2) = '" + commonMatcher.group(2) + "' group(3) = '" + commonMatcher.group(3) + "'");
+					
 					Pattern commandPattern = Pattern.compile(commonMatcher.group(2));
 					Matcher commandMatcher = commandPattern.matcher(command);
 					Log.d(MainActivity.TAG, this.getLocalClassName() + ": commandMatcher = '" + commandMatcher.matches() + "'");
-					if (commandMatcher.matches()){
-						Log.d(MainActivity.TAG, this.getLocalClassName() + ": group(1) = '" + commandMatcher.group(1) + 
-								"'");
+					
+					if (commandMatcher.matches()) {
+						Log.d(MainActivity.TAG, this.getLocalClassName() + ": group(1) = '" + commandMatcher.group(1) + "'");
+						
 						Intent resIntent = new Intent();
 						setResult(RESULT_OK, resIntent);
 						resIntent.putExtra("command", command); 
 
 				        Toast.makeText(this, "Matches some pattern", Toast.LENGTH_LONG).show();
+				        
+						// Applications' launcher should be here? 
 					    
 						finish();
 					}
 				} else {
-					Log.d(MainActivity.TAG, this.getLocalClassName() + ": Pattern is incorrect");
+					Log.d(MainActivity.TAG, this.getLocalClassName() + ": Pattern '" + rawPattern + "' is incorrect");
 				}
 			}
+			
 			Intent resIntent = new Intent();
 			setResult(RESULT_OK, resIntent);
 			resIntent.putExtra("command", command); 
@@ -70,6 +72,7 @@ public class CommandAnalyser extends Activity {
 	        Toast.makeText(this, "No matches", Toast.LENGTH_LONG).show();
 		    
 			finish();
+			
 		} catch (IOException e) {
 			Intent resIntent = new Intent();
 			setResult(RESULT_CANCELED, resIntent);
@@ -78,6 +81,7 @@ public class CommandAnalyser extends Activity {
 			Log.e(MainActivity.TAG, "Reading file with patterns failed", e);
 			
 			finish();
+			
 		} finally {
 			try {
 				patternsReader.close();
@@ -92,30 +96,6 @@ public class CommandAnalyser extends Activity {
 			}
 		}
 		
-		// Direct use of Pattern:
-		/*Pattern phoneDialPattern_1 = Pattern.compile("позвони");
-		Matcher m = phoneDialPattern_1.matcher(command);
-
-		Log.d(MainActivity.TAG, this.getLocalClassName() + ": m.matches() = '" + m.matches() + "'");
-		
-		if (m.matches()) {
-			// Log.d(MainActivity.TAG, this.getLocalClassName() + ": m.group(1) = '" + m.group(1) + "'");
-			
-			Uri number = Uri.parse("tel:100");
-			Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
-			startActivityForResult(callIntent, COMMAND_ANALYSER_REQUEST_CODE);
-			
-		}
-		else {
-			Intent resIntent = new Intent();
-			setResult(RESULT_OK, resIntent);
-			resIntent.putExtra("command", command); 
-
-	        Toast.makeText(this, "No matches", Toast.LENGTH_LONG).show();
-		    
-			finish();	
-		}*/
-
 	}
 	
 	
